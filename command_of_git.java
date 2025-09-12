a04e4b6759ed7ce88f5cdc8ba3971da1a867f7ac
Sahel222 <123066137+Sahel222@users.noreply.github.com>
2025-09-13 02:26:01 +0600

    new feature1

--- command_of_git.java
+++ command_of_git.java
@@ -1,5 +1,7 @@
 /*
  * cd -> Change Directory
+ * cd .. -> get out of the directory 
+ * mkdir -> to make new directory
  * clear -> clear the terminal
  * ls -> shows list of files
  * ls -Force -> show all files including hidden files.
@@ -18,14 +20,24 @@
  * git add .                                 -> add all changes 
  * git add <file name>                       -> git add index.html
  * git commit -m "some message"              -> git commit -m "added index.html"
- * git push origin main                      -> Local system to main branch code published
+ * git push origin main                      -> Local system to <main branch> code published
+ * git push -u origin main                   -> Now for the future we have fixed the origin main, now "git push" means "git push origin main"
+ * git branch                                -> To check which branch we are in
+ * git branch -M <new branch name>           -> Change the name of the old branch 
+ * git checkout <branch name>                -> To go in another branch
+ * git checkout -b <New Branch Name>         -> Create a new branch
+ * git branch -d <Branch Name>             -> Delete a branch (But first get out of that branch)
+ * 
+ * 
+ * 
+ * git init                                  -> initialize repo(.git) in and exisiting code file
+ * git remote add origin <link>              -> Create a new repo in github then take the link
+ * git remote -v                             -> to verify remote
  * 
  * 
  * 
  * 
- * 
- * 
- * to use git commands we have to cd into the folder of git that we cloned at first.
+ * to use git commands we have to cd into the folder of git that we cloned at first.  (PS C:\Users\User\Desktop\Git\Know-Git> )
  * staged means ready to commit.
  * untracked means new file created but added till now.
  * modified means there are some chnages happend in a file.
